03f91ec11b32d1df70d68dde13bcdab5989a2211
ramber-diff <125486132+ramber-diff@users.noreply.github.com>
2023-02-17 12:29:11 +0000

    Update streamlit_test_V2.py

--- streamlit_test_V2.py
+++ streamlit_test_V2.py
@@ -17,3 +17,6 @@ def run_query(query):
         cur.execute(query)
         return cur.fetch_pandas_all()
 
+df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
+
+st.dataframe(df)
